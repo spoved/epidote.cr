3f6ae01e3e27271c59ebf3db9c94d86bb0c66594
Holden Omans <holden.omans@gmail.com>
2020-09-14 17:49:05 -0400

    update

--- src/epidote/adapter/mysql.cr
+++ src/epidote/adapter/mysql.cr
@@ -5,10 +5,10 @@ class Epidote::Adapter::MySQL < Epidote::Adapter
   alias DataHash = Hash(String, Array(JSON::Any) | Bool | Float64 | Hash(String, JSON::Any) | Int64 | String | Nil)
 
   OPTIONS = HTTP::Params.new({
-    "retry_attempts"     => ["8"],
-    "retry_delay"        => ["3"],
-    "max_pool_size"      => ["2"],
-    "max_idle_pool_size" => ["0"],
+    "retry_attempts"     => [ENV.fetch("MYSQL_DB_RETRY_ATTEMPTS", "8")],
+    "retry_delay"        => [ENV.fetch("MYSQL_DB_RETRY_DELAY", "3")],
+    "max_pool_size"      => [ENV.fetch("MYSQL_DB_MAX_POOL_SIZE", "5")],
+    "max_idle_pool_size" => [ENV.fetch("MYSQL_DB_IDLE_POOL_SIZE", "0")],
   })
 
   MYSQL_DB_NAME = ENV["CRYSTAL_ENV"]? ? "#{ENV["MYSQL_DB_NAME"]}_#{ENV["CRYSTAL_ENV"]?}" : "#{ENV["MYSQL_DB_NAME"]}"
